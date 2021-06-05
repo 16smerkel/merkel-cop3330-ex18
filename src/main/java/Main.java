fbdf3d3b3e37aaca1331f442dd4b33fc020575dc
16smerkel <16smerkel@gmail.com>
2021-06-04 23:50:20 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Press C to convert from Fahrenheit to Celsius.");
+        System.out.println("Press F to convert from Celsius to Fahrenheit.");
+        System.out.println("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        String option = scanner.nextLine();
+        while (!option.equals("c") && !option.equals("C") && !option.equals("f") && !option.equals("F")) {
+            System.out.println("Please enter either C or F");
+            option = scanner.nextLine();
+        }
+        if (option.equals("c") || option.equals("C")) {
+            System.out.println("Please enter the temperature in Fahrenheit: ");
+            double theFah = scanner.nextDouble();
+            double theCel = ((theFah - 32) * 5 / 9);
+            System.out.println("The temperature in Celsius is " + theCel);
+
+        } else if (option.equals("f") || option.equals("F")) {
+            System.out.println("Please enter the temperature in Celsius: ");
+            double theCel = scanner.nextDouble();
+            double theFah = ((theCel * 9 / 5) + 32);
+            System.out.println("The temperature in Fahrenheit is " + theFah);
+
+        }
+    }
+}
